755b23d9d25ec4227a14f1784cd98e07819568c8
Heitor Loguércio Perrotta <54395989870@info.local>
2024-07-26 17:25:18 -0300

    4 termo

---
+++ T4/minimalApiMongo/minimalApiMongo/Controller/ProductController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using minimalApiMongo.Domains;
+using minimalApiMongo.Services;
+using MongoDB.Driver;
+
+namespace minimalApiMongo.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMongoCollection<Product> _product;
+
+        public ProductController(MongoDbService mongoDbService)
+        {
+            _product = mongoDbService.GetDatabase.GetCollection<Product>("product");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            try
+            {
+                var products = await _product.Find(FilterDefinition<Product>.Empty).ToListAsync();
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Product p)
+        {
+            try
+            {
+                Product novoProduto = new Product()
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    AdditionalAtrributes = p.AdditionalAtrributes,
+                };
+
+                await _product.InsertOneAsync(novoProduto);
+                return Ok(novoProduto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(string id)
+        {
+            try
+            {
+                await _product.DeleteOneAsync(p => p.Id == id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(string id, Product product)
+        {
+
+            try
+            {
+                var filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+                var res = await _product.Find(filter).ToListAsync();
+
+                if (product.Name == null)
+                {
+                    product.Name = res.First().Name;
+                }
+
+                if (product.Price == 0)
+                {
+                    product.Price = res.First().Price;
+                }
+
+                var update = Builders<Product>.Update
+                    .Set(p => p.Name, product.Name)
+                    .Set(p => p.Price, product.Price);
+
+                await _product.UpdateOneAsync(filter, update);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(string id)
+        {
+            try
+            {
+                var filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+                var result = _product.Find(filter);
+
+                return Ok(result.First());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ T4/minimalApiMongo/minimalApiMongo/Services/MongoDbService.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+
+namespace minimalApiMongo.Services
+{
+    public class MongoDbService
+    {
+        /// <summary>
+        /// Armazenar a configuração da aplicação
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Armazenar a referência ao MongoDb
+        /// </summary>
+        private readonly IMongoDatabase _database;
+
+        /// <summary>
+        /// Contém a configuração necessária paa acesso ao MongoDb
+        /// </summary>
+        /// <param name="configuration">Objeto com a config da aplicação</param>
+        public MongoDbService(IConfiguration configuration)
+        {
+            //Atriubui a config recebida em _configuration
+            _configuration = configuration;
+
+            //Acessa a String de conexão
+            var connectionString = _configuration.GetConnectionString("DbConnection");
+
+            //Transforma a string obtida em MongoUrl
+            var mongoUrl = MongoUrl.Create(connectionString);
+
+            //Cria um client
+            var mongoClient = new MongoClient(mongoUrl);
+
+            //Obtém a referência ao MongoDb
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+        /// <summary>
+        /// Propriedade para acessar o bd => retorna os dados em _database
+        /// </summary>
+        public IMongoDatabase GetDatabase => _database;
+
+    }
+}
